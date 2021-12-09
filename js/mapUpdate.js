6ce0c03473a3b2a933f1a632092f14603a3f0760
Suvraneel Bhuin <bsuvraneel@gmail.com>
2021-12-09 16:29:57 +0530

    UI sync init

--- js/mapUpdate.js
+++ js/mapUpdate.js
@@ -1,8 +1,8 @@
 const mapIfrm = document.getElementById('mapIframe');
+const googleMapAPI = 'AIzaSyAPPopCJHxOtwtsGokOpoKNRnWCq4XRH-k';
 function mapUpdateIframe(city) {
     mapIfrm.innerHTML = 
-    `<iframe id="mapIframe" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAPPopCJHxOtwtsGokOpoKNRnWCq4XRH-k&q=${city}" width="100%" height="600" style="filter: invert(100%)"></iframe>
+    `<iframe id="mapIframe" src="https://www.google.com/maps/embed/v1/place?key=${googleMapAPI}&q=${city}" width="100%" height="600" style="filter: invert(100%)"></iframe>
     `;
     // console.log(latitude, longitude);
-    console.log(mapIfrm.innerHTML);
 }
--- js/openWeatherAPI.js
+++ js/openWeatherAPI.js
@@ -148,27 +148,14 @@ function showWeatherData (data){
 
     let otherDayForcast = ''
     data.daily.forEach((day, idx) => {
-        if(idx == 0){
-            currentTempEl.innerHTML = `
-            <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
-            <div class="other small"><br/>
-                <div class="day h5">${window.moment(day.dt*1000).format('dddd')}</div>
-                <div class="temp">Night - ${day.temp.night}&#176;C</div>
-                <div class="temp">Day - ${day.temp.day}&#176;C</div>
-            </div>
-            
-            `
-        }else{
             otherDayForcast += `
             <div class="weather-forecast-item">
                 <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">Night - ${day.temp.night}&#176;C</div>
-                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+                <div class="temp">${day.temp.min}&#176;C/${day.temp.max}&#176;C</div>
             </div>
             
             `
-        }
     })
 
 
